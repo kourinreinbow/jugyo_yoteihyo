1b38c2a985bbcf490ea6735fe1650e9e126c08c1
Koumasa <koumasarein@gmail.com>
2023-04-19 18:17:40 +0900

    first commit

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ kadai_datas = \
 [
  ["ディジタル回路演習：レポート", datetime.datetime(2023,5,1,0,0)],
  ["応用数学Ⅱ：課題01", datetime.datetime(2023,4,23,23,59)],
- ["応用数学Ⅱ：課題02", datetime.datetime(2023,4,23,23,59)]
+ ["応用数学Ⅱ：課題02", datetime.datetime(2023,4,23,23,59)],
+ ["情報理論：第1章,章末問題", datetime.datetime(2023,5,8)],
+ ["ソフトウェア工学：PERT図", datetime.datetime(2023,4,25,13,0)],
+ [" データベース：第２回課題", datetime.datetime(2023,4,25)],
+ ["工学実験Ⅴ：第１回分", datetime.datetime(2023,4,20,8,40)],
+ ["応用物理Ⅱ：プリント課題", "次回の授業まで"]
  ]
 
 template_html = \
 """
 <tr>
     <th>{}</th>
-    <td>{}月{}日{}時{}分</td>
-    <td><form method="GET"><input type="checkbox" name="kadai{}">　<input type="submit" value="変更"></form></td>
+    <td>{}/{:02d}/{:02d}/{:02d}:{:02d}</td>
+    <!--<td><form method="GET"><input type="checkbox" name="kadai{}">　<input type="submit" value="変更"></form></td>-->
+</tr>
+"""
+
+template_html_jikaiVer = \
+"""
+<tr>
+    <th>{}</th>
+    <td>{}</td>
+    <!--<td><form method="GET"><input type="checkbox" name="kadai{}">　<input type="submit" value="変更"></form></td>-->
 </tr>
 """
 
@@ -24,7 +38,10 @@ f_index_css = open("./index.css", "w", encoding="utf-8")
 write_html = ""
 for index, kadai_data in enumerate(kadai_datas):
     date:datetime.datetime = kadai_data[1]
-    write_html += template_html.format(kadai_data[0], date.year, date.month, date.hour, date.minute, index)
+    if type(date) == str:
+        write_html += template_html_jikaiVer.format(kadai_data[0], date, index)
+    else:
+        write_html += template_html.format(kadai_data[0], date.year, date.month, date.day, date.hour, date.minute, index)
 
 # テンプレートのデータを読み込む
 f_sample_html_data = f_sample_html.read()
